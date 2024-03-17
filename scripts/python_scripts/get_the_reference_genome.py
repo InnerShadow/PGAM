cfcb69b850fcb6d6fde4f5eddc69fb2e37854a66
Nobody <mishasikolenko@mail.ru>
2024-03-17 18:28:45 +0300

    Update getting reference genome

--- scripts/python_scripts/get_the_reference_genome.py
+++ scripts/python_scripts/get_the_reference_genome.py
@@ -3,6 +3,7 @@ import os
 from Bio import Entrez
 
 def get_the_reference_genome(assembly_id, email, output_folder_id):
+    Entrez.email = args.email
 
     output_path = os.path.join("data/samples/", f"sample_{output_folder_id}")
     os.makedirs(output_path, exist_ok = True)
@@ -28,8 +29,6 @@ if __name__ == "__main__":
     parser.add_argument("email", type = str, help = "Your email address")
     parser.add_argument("output_folder_id", type = int, help = "Output folder id to save the data")
     args = parser.parse_args()
-
-    Entrez.email = args.email
     
     get_the_reference_genome(args.assembly_id, args.email, args.output_folder_id)
 
