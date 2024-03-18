0e8e07cd21114b4b86f126f353d9a64911d5c8a1
Nobody <mishasikolenko@mail.ru>
2024-03-18 18:17:59 +0300

    Better bash

--- scripts/bash_scripts/create_dataset.sh
+++ scripts/bash_scripts/create_dataset.sh
@@ -74,8 +74,8 @@ while IFS= read -r line || [ -n "$line" ]; do
                 fastp -i "${read_id}.fastq" -o "${read_id}_fastp.fastq" -h "${read_id}_fastp_report.html"
 
                 # Align reads to the reference genome & save align results to .txt file
-                bowtie2 --local -p 6 -x "../index/${reference_genome}_index" -U "${read_id}_fastp.fastq" \
-                    | samtools view -O BAM -b -o "${read_id}_fastp_mapping.bam" 2> "${read_id}_alignments_report.txt"
+                (bowtie2 --local -p 6 -x "../index/${reference_genome}_index" -U "${read_id}_fastp.fastq" \
+                    | samtools view -O BAM -b -o "${read_id}_fastp_mapping.bam") 2> "${read_id}_alignments_report.txt"
 
                 # Sort alignments
                 samtools sort -@ 6 -O BAM -o "${read_id}_fastp_mapping_sorted.bam" "${read_id}_fastp_mapping.bam"
