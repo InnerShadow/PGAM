ffa5b8be45646e2ba7b9018dd6e1cd8a5b595f6d
Nobody <mishasikolenko@mail.ru>
2024-03-21 14:13:40 +0300

    Clean dataset

--- scripts/python_scripts/get_the_reference_genome.py
+++ scripts/python_scripts/get_the_reference_genome.py
@@ -2,10 +2,10 @@ import argparse
 import os
 from Bio import Entrez
 
-def get_the_reference_genome(assembly_id, email, output_folder_id):
+def get_the_reference_genome(assembly_id, email):
     Entrez.email = email
 
-    output_path = os.path.join("../", f"sample_{output_folder_id}")
+    output_path = os.path.join(".", "")
     os.makedirs(output_path, exist_ok = True)
 
     genome_handle = Entrez.efetch(db = "nucleotide", id = assembly_id, rettype = "fasta", retmode = "text")
@@ -27,10 +27,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Download reference genome and annotation")
     parser.add_argument("assembly_id", type = str, help = "NCBI GenBank ID")
     parser.add_argument("email", type = str, help = "Your email address")
-    parser.add_argument("output_folder_id", type = int, help = "Output folder id to save the data")
+    # parser.add_argument("output_folder_id", type = int, help = "Output folder id to save the data")
     args = parser.parse_args()
     
-    get_the_reference_genome(args.assembly_id, args.email, args.output_folder_id)
+    get_the_reference_genome(args.assembly_id, args.email)
 
     print(f"completing the download of {args.assembly_id} files!")
 
